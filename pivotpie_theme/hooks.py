c8e7d52bd4bb88089227804d93352f8b7e96df03
ajaykrishnancv <142998405+ajaykrishnancv@users.noreply.github.com>
2024-02-22 11:06:15 +0400

    changes

--- pivotpie_theme/hooks.py
+++ pivotpie_theme/hooks.py
@@ -10,11 +10,11 @@ app_license = "mit"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/pivotpie_theme/css/pivotpie_theme.css"
+app_include_css = "/assets/pivotpie_theme/css/pivotpie_theme.css"
 # app_include_js = "/assets/pivotpie_theme/js/pivotpie_theme.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/pivotpie_theme/css/pivotpie_theme.css"
+web_include_css = "/assets/pivotpie_theme/css/pivotpie_theme.css"
 # web_include_js = "/assets/pivotpie_theme/js/pivotpie_theme.js"
 
 # include custom scss in every website theme (without file extension ".scss")
